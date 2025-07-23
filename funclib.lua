8cd7346666e1a115b747da8ce18d48e132f2a041
yixiyotatsuki <161025930+yixiyotatsuki@users.noreply.github.com>
2025-07-23 16:14:40 +0800

    Update funclib.lua
    funclib:GetTool was erroring when the character was nil

--- funclib.lua
+++ funclib.lua
@@ -55,7 +55,9 @@ function module:fireRemote(remote,args) -- fires a remote
 end
 
 function module:GetTool(name) -- searches for the tool in the players inventory or the players character (holding item)
-    return game:GetService("Players").LocalPlayer:WaitForChild("Backpack"):FindFirstChild(name) or game:GetService("Players").LocalPlayer.Character:FindFirstChild(name)
+    local object=return game:GetService("Players").LocalPlayer:WaitForChild("Backpack"):FindFirstChild(name)
+    if object==nil and game:GetService("Players").LocalPlayer.Character~=nil then game:GetService("Players").LocalPlayer.Character:FindFirstChild(name) end
+    return object
 end
 
 return module
